05d1e9a56edf727b4c576e360086834693f1867d
Mark <mark.n.bacon.jr@gmail.com>
2022-05-26 14:53:36 -0500

    fixed typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,4 @@ class User < ApplicationRecord
     has_many :favorite_cards
     has_many :credit_cards, through: :favorite_cards
 
-    def create 
-        # This is where the user account should be created!
-        
-    end
-
-    # Login and Logout needs to go in Sessions controller
-    #secret page that you can only see if you're logged in
-    private 
-
-    def user_params
-        params.permit(:password, :email, :age, :name)
-        # password digest?
-    end
 end
