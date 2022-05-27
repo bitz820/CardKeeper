d6281a1a54c7ccb2826aa15069e54a831928f787
Mark <mark.n.bacon.jr@gmail.com>
2022-05-27 13:14:26 -0500

    ok actaully finally fixed client side routing...

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'
+import {BrowserRouter as Router} from "react-router-dom"
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
+    <Router>
     <App />
-  </React.StrictMode>
+    </Router>
+  </React.StrictMode>,
+document.getElementById('root')
 );
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
-  post "/logout", to: "sessions#destroy"
+  delete "/logout", to: "sessions#destroy"
   get "/auth", to: "users#show"
   # Defines the root path route ("/")
   # root "articles#index"
